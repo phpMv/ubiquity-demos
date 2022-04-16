3834d219d7c5f0e8636bf3fce4c8d3611822cc83
jcheron <myaddressmail@gmail.com>
2022-04-16 11:55:27 +0200

    update OAuth models

--- oauth-project/app/models/Authtokens.php
+++ oauth-project/app/models/Authtokens.php
@@ -1,32 +1,48 @@
 <?php
 namespace models;
+use Ubiquity\attributes\items\Column;
+use Ubiquity\attributes\items\Id;
+use Ubiquity\attributes\items\Table;
+use Ubiquity\attributes\items\Transformer;
+use Ubiquity\attributes\items\Validator;
+
 /**
  * @table('authtokens')
 */
+#[Table('authtokens')]
 class Authtokens{
 	/**
 	 * @id
 	 * @column("name"=>"id","nullable"=>false,"dbType"=>"int(11)")
 	 * @validator("id","constraints"=>array("autoinc"=>true))
 	**/
+	#[Id]
+	#[Column('id',nullable: false,dbType: 'int(11)')]
+	#[Validator('id',constraints: ['autoinc'=>true])]
 	private $id;
 
 	/**
 	 * @column("name"=>"selector","nullable"=>false,"dbType"=>"char(24)")
 	 * @validator("length","constraints"=>array("max"=>24,"notNull"=>true))
 	**/
+	#[Column('selector',nullable: false,dbType: 'char(24)')]
+	#[Validator('length',constraints: ['max'=>24,'notNull'=>true])]
 	private $selector;
 
 	/**
 	 * @column("name"=>"hashedValidator","nullable"=>false,"dbType"=>"char(64)")
 	 * @validator("length","constraints"=>array("max"=>64,"notNull"=>true))
 	**/
+	#[Column('hashedValidator',nullable: false,dbType: 'char(64)')]
+	#[Validator('length',constraints: ['max'=>64,'notNull'=>true])]
 	private $hashedValidator;
 
 	/**
 	 * @column("name"=>"userid","nullable"=>false,"dbType"=>"int(11)")
 	 * @validator("notNull")
 	**/
+	#[Column('userId',nullable: false,dbType: 'int(11)')]
+	#[Validator('notNull')]
 	private $userid;
 
 	/**
@@ -34,6 +50,9 @@ class Authtokens{
 	 * @validator("type","dateTime")
 	 * @transformer("name"=>"datetime")
 	**/
+	#[Column('expires',nullable: true,dbType: 'datetime')]
+	#[Validator('dateTime')]
+	#[Transformer('datetime')]
 	private $expires;
 
 	 public function getId(){
@@ -76,7 +95,7 @@ class Authtokens{
 		$this->expires=$expires;
 	}
 
-	 public function __toString(){
+	 public function __toString(): string {
 		return ($this->userid??'no value').'';
 	}
 
--- oauth-project/app/models/User.php
+++ oauth-project/app/models/User.php
@@ -1,26 +1,41 @@
 <?php
 namespace models;
+use Ubiquity\attributes\items\Column;
+use Ubiquity\attributes\items\Id;
+use Ubiquity\attributes\items\Table;
+use Ubiquity\attributes\items\Transformer;
+use Ubiquity\attributes\items\Validator;
+
 /**
  * @table('user')
 */
+#[Table('user')]
+
 class User{
 	/**
 	 * @id
 	 * @column("name"=>"id","nullable"=>false,"dbType"=>"int(11)")
 	 * @validator("id","constraints"=>array("autoinc"=>true))
 	**/
+	#[Id]
+	#[Column('id',nullable: false,dbType: 'int(11)')]
+	#[Validator('id',constraints: ['autoinc'=>true])]
 	private $id;
 
 	/**
 	 * @column("name"=>"name","nullable"=>false,"dbType"=>"varchar(30)")
 	 * @validator("length","constraints"=>array("max"=>30,"notNull"=>true))
 	**/
+	#[Column('name',nullable: false,dbType: 'char(30)')]
+	#[Validator('length',constraints: ['max'=>30,'notNull'=>true])]
 	private $name;
 
 	/**
 	 * @column("name"=>"login","nullable"=>false,"dbType"=>"varchar(30)")
 	 * @validator("length","constraints"=>array("max"=>30,"notNull"=>true))
 	**/
+	#[Column('login',nullable: false,dbType: 'char(30)')]
+	#[Validator('length',constraints: ['max'=>30,'notNull'=>true])]
 	private $login;
 
 	/**
@@ -28,12 +43,17 @@ class User{
 	 * @validator("length","constraints"=>array("max"=>128,"notNull"=>true))
 	 * @transformer("name"=>"password")
 	**/
+	#[Column('password',nullable: false,dbType: 'char(128)')]
+	#[Validator('length',constraints: ['max'=>128,'notNull'=>true])]
+	#[Transformer('password')]
 	private $password;
 
 	/**
 	 * @column("name"=>"oauth","nullable"=>true,"dbType"=>"varchar(32)")
 	 * @validator("length","constraints"=>array("max"=>32))
 	**/
+	#[Column('oauth',nullable: true,dbType: 'char(32)')]
+	#[Validator('length',constraints: ['max'=>32])]
 	private $oauth;
 
 	 public function getId(){
@@ -76,7 +96,7 @@ class User{
 		$this->oauth=$oauth;
 	}
 
-	 public function __toString(){
+	 public function __toString(): string {
 		return ($this->login??'no value').'';
 	}
 

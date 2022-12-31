69c2bed6b63e4e0f837963200de9c6a61b78e10a
jcheron <myaddressmail@gmail.com>
2022-12-31 11:29:40 +0100

    Update services.php

--- acl-db-project/app/config/services.php
+++ acl-db-project/app/config/services.php
@@ -7,6 +7,11 @@ Router::start();
 Router::addRoute("_default", \controllers\MainController::class);
 \Ubiquity\security\acl\AclManager::start();
 \Ubiquity\security\acl\AclManager::initFromProviders([
-	new \Ubiquity\security\acl\persistence\AclDAOProvider($config),
+	new \Ubiquity\security\acl\persistence\AclDAOProvider($config, [
+		'acl'=>\models\AclElement::class,
+		'role'=>\models\Role::class,
+		'resource'=>\models\Resource::class,
+		'permission'=>\models\Permission::class
+	]),
 	new \Ubiquity\security\acl\persistence\AclCacheProvider()
 ]);
